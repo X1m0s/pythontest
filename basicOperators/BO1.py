a603613b5a48e0357aae8b28224570341ff23eec
ximo <irximos@gmail.com>
2026-06-23 14:54:12 +0800

    运算符

--- basicOperators/BO1.py
+++ basicOperators/BO1.py
@@ -34,19 +34,22 @@ else:
     print("a>b")
 #-----3.赋值运算符-----
 a=21;b=10;c=0
-c=a+b
-print("c=",c)
-c+=a
-print ("c=",c)
-c*=a
-print ("c=",c)
-c/=a
-print ("c=",c)
+c=a+b;print("c=",c)
+c+=a;print ("c=",c)
+c*=a;print ("c=",c)
+c/=a;print ("c=",c)
 
 c=2
-c%=a
-print ("c=",c)
-c**=a
-print ("c=",c)
-c//=a
-print ("c=",c)
+c%=a;print ("c=",c)
+c**=a;print ("c=",c)
+c//=a;print ("c=",c)
+#python3.8中新引入了海象运算符(Walrus Operator)，符号为:=，主要用在表达式中同时进行赋值和返回赋值的值
+#可用于简化循环条件和表达式中的重复计算
+#传统写法如下
+n=10
+if n>5:
+    print(n)
+#使用海象运算符
+if(n:=10)>5:            #海象运算符在表达式中进行赋值操作，将变量n赋值为10，同时返回赋值结果
+    print(n)
+#海象运算符会降低代码可读性，在使用时必须用括号包裹，在f-string中也一样
